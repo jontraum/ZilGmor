39a0ee0ca0d2091c48e9906b9922d3b3810fb94e
Jonathan Traum <jont@traumatic.us>
2023-07-05 19:00:51 -0400

    Early version of a book view, with hardcoded book content.

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Platform } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+import { useState } from 'react';
+import { SefariaTextPage } from './SefariaTextPage';
 
 export default function App() {
+  const [size, setSize] = useState(12);
+  const textStyle = {fontSize: size};
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <View style={styles.tools}>
+          <AntDesign name="pluscircleo" size={36} color="black" onPress={() => setSize(size+1)}/>
+          <AntDesign name="minuscircleo" size={36} color="black" onPress={() => setSize(size-1)}/>
+      </View> */}
+      <SefariaTextPage />
     </View>
   );
 }
@@ -16,5 +24,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 40,
   },
+  tools: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 16,
+    marginTop: 10,
+  }
 });
---
+++ SefariaTextItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions } from "react-native";
+import RenderHtml from 'react-native-render-html';
+// Keeping these old imports to remember these alternatives, in case I want to go back to them later
+// If you're still reading this after the end of 2023, should probably just get rid of these!
+// import {WebView} from 'react-native-webview';
+// import AutoHeightWebView from 'react-native-autoheight-webview';
+
+interface SefariaTextItemProps {
+    textHE: string;
+    textEN: string;
+    itemNumber: number;
+}
+
+const padding = 1;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "space-between",
+      padding: padding,
+    },
+    textView: {
+        flex: 1,
+        // height: 60,
+        width: Dimensions.get('window').width - (padding * 2),
+        fontSize: 16,
+    },
+    wvContainer: {
+        fontSize: 30
+    }
+});
+
+export function SefariaTextItem({textHE, textEN}: SefariaTextItemProps) {
+    return (
+        <View style={styles.container}>
+            <RenderHtml contentWidth={styles.textView.width} source={{html: textHE}} />
+            <RenderHtml contentWidth={styles.textView.width} source={{html: textEN}} />
+            {/* <AutoHeightWebView style={styles.textView} source={{html: textHE, baseUrl: "http://sefaria.org/"}} originWhitelist={['*']} />
+            <AutoHeightWebView textZoom={100} style={styles.textView} source={{html: textEN, baseUrl: "http://sefaria.org/"}} originWhitelist={['*']} /> */}
+        </View>
+    )
+}
