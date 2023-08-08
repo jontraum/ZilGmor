4b78907e60394183df2d796c67414b5745589174
Jonathan Traum <jont@traumatic.us>
2023-08-08 18:58:49 -0400

    Upgraded to new versions of react-native and expo. Fixed package issues introduced when adding eslint.

--- App.js
+++ App.js
@@ -6,44 +6,44 @@ import { bavli } from './data/bavli'
 // import Constants from 'expo-constants';
 
 export default function App () {
-    const [currentBook, setCurrentBook] = useState()
-    //const [size, setSize] = useState(12)
-    //const textStyle = { fontSize: size }
-    if (currentBook) {
-        return (
-            <View style={styles.container}>
-                {/* <View style={styles.tools}>
+  const [currentBook, setCurrentBook] = useState()
+  //const [size, setSize] = useState(12)
+  //const textStyle = { fontSize: size }
+  if (currentBook) {
+    return (
+      <View style={styles.container}>
+        {/* <View style={styles.tools}>
             <AntDesign name="pluscircleo" size={36} color="black" onPress={() => setSize(size+1)}/>
             <AntDesign name="minuscircleo" size={36} color="black" onPress={() => setSize(size-1)}/>
         </View> */}
-                <SefariaTextPage
-                    currentBook={currentBook}
-                    goToLibrary={() => { setCurrentBook(null) }}
-                />
-            </View>
-        )
-    } else {
-        return (
-            <View style={styles.container}>
-                <Text>Here are some books!</Text>
-                <LibrarySection bookList={bavli} setCurrentBook={setCurrentBook} />
-            </View>
-        )
-    }
+        <SefariaTextPage
+          currentBook={currentBook}
+          goToLibrary={() => { setCurrentBook(null) }}
+        />
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Here are some books!</Text>
+        <LibrarySection bookList={bavli} setCurrentBook={setCurrentBook} />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: StatusBar.currentHeight // or maybe from Expo: Constants.statusBarHeight,
-    },
-    tools: {
-        flex: 1,
-        flexDirection: 'row',
-        height: 16,
-        marginTop: 10
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight, // or maybe from Expo: Constants.statusBarHeight,
+  },
+  tools: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 16,
+    marginTop: 10,
+  },
 })
